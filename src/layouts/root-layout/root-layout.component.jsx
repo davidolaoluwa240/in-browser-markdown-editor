8c25609e61b8a83bfcab84d58ab800d66fa3eb90
Atebisun David <davidolaoluwa240@gmail.com>
2023-06-11 20:51:16 +0200

    Fixed issue with the handleOpenSettingsModal function

--- src/layouts/root-layout/root-layout.component.jsx
+++ src/layouts/root-layout/root-layout.component.jsx
@@ -67,7 +67,7 @@ const _RootLayout = () => {
   const handleOpenSettingsModal = useCallback(() => {
     setIsSettingsModalOpen(true);
     handleToggleMenuVisibility();
-  }, []);
+  }, [isSideBarOpen]);
 
   /**
    * Close Settings Modal
